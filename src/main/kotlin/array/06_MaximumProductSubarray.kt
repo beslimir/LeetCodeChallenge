8c09919c984ac30179ac6f79bc7f436e03530297
beslimir <marin.beslo@gmail.com>
2022-03-12 20:56:57 +0100

    array task 6 - update

--- src/main/kotlin/array/06_MaximumProductSubarray.kt
+++ src/main/kotlin/array/06_MaximumProductSubarray.kt
@@ -1,21 +1,25 @@
 package array
 
 import kotlin.math.max
+import kotlin.math.min
 
 fun main(args: Array<String>) {
-    val nums = intArrayOf(11, 2, -3, -4, 0, 7)
+    val nums = intArrayOf(2,0,3,-2,-2,4)
 
     println(maxSubarrayProduct(nums))
 }
 
 fun maxSubarrayProduct(nums: IntArray): Int {
     var max = nums[0]
+    var min = nums[0]
     var curr = nums[0]
 
     for (i in 1 until nums.size) {
-        curr = max(curr * nums[i], nums[i])
-        max = max(curr, max)
+        var temp = min
+        min = min(nums[i], min(nums[i] * min, nums[i] * max))
+        max = max(nums[i], max(nums[i] * max, nums[i] * temp))
+        curr = max(max, curr)
     }
 
-    return max
+    return curr
 }
