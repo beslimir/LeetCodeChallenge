778506ca87ff90b4a77a2f99cf1e1fcc9fd7a622
beslimir <marin.beslo@gmail.com>
2022-03-13 22:03:26 +0100

    binary task 1

---
+++ src/main/kotlin/binary/01_SumTwoIntegers.kt
@@ -0,0 +1,24 @@
+package binary
+
+fun main(args: Array<String>) {
+    val a = 3
+    val b = 5
+
+    println(sumTwoIntegers(a, b))
+}
+
+fun sumTwoIntegers(x1: Int, x2: Int): Int {
+    var a = x1
+    var b = x2
+
+    while (b != 0) {
+        val tmp = (a.and(b)).shl(1)
+        println(tmp)
+        a = a.xor(b)
+        println(a)
+        b = tmp
+        println(b)
+    }
+
+    return a
+}
