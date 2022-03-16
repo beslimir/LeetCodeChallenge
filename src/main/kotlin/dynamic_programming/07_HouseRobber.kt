ea4fcef21896646e8f74c4da81162bf2351c2179
beslimir <marin.beslo@gmail.com>
2022-03-16 21:30:41 +0100

    dynamic programming task 7

---
+++ src/main/kotlin/dynamic_programming/07_HouseRobber.kt
@@ -0,0 +1,21 @@
+package dynamic_programming
+
+import kotlin.math.max
+
+fun main(args: Array<String>) {
+    println(houseRobber(intArrayOf(2, 7, 9, 3, 1)))
+}
+
+fun houseRobber(nums: IntArray): Int {
+    var robbery1 = 0
+    var robbery2 = 0
+
+    //[robbery1, robbery2, n, n + 1,...]
+    for(n in nums) {
+        val temp = max(n + robbery1, robbery2)
+        robbery1 = robbery2
+        robbery2 = temp
+    }
+
+    return robbery2
+}
