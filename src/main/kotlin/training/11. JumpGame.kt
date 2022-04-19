0dfebaf70c34994abf19b9ed9ad45676524b5551
beslimir <marin.beslo@gmail.com>
2022-04-19 10:43:08 +0200

    training: DP 11

---
+++ src/main/kotlin/training/11. JumpGame.kt
@@ -0,0 +1,17 @@
+package training
+
+fun main(args: Array<String>) {
+    println(jumpGame(intArrayOf(3, 2, 1, 0, 4)))
+}
+
+fun jumpGame(nums: IntArray): Boolean {
+    var goal = nums.size - 1
+
+    for (i in nums.size - 1 downTo 0) {
+        if (i + nums[i] >= goal) {
+            goal = i
+        }
+    }
+
+    return goal == 0
+}
