8d48e48c9c706db6ad39093aad2a217bc5dc6a92
beslimir <marin.beslo@gmail.com>
2022-04-25 18:42:35 +0200

    New users challenges 04 -> new solution

--- src/main/kotlin/other/FizzBuzz.kt
+++ src/main/kotlin/other/FizzBuzz.kt
@@ -1,7 +1,7 @@
 package other
 
 fun main(args: Array<String>) {
-    println(fizzBuzz2(15))
+    println(fizzBuzz3(15))
 }
 
 fun fizzBuzz(n: Int): List<String> {
@@ -35,4 +35,33 @@ fun fizzBuzz2(n: Int): List<String> {
     }
 
     return myList
+}
+
+fun fizzBuzz3(n: Int): List<String> {
+    val res = Array(n) { "why" }
+    var r = if (n % 2 == 0) {
+        (n / 2)
+    } else {
+        (n / 2) + 1
+    }
+    var l = 1
+
+    for (l in 1..n / 2 + 1) {
+        when {
+            l % 3 == 0 && l % 5 == 0 -> res[l - 1] = "FizzBuzz"
+            l % 5 == 0 -> res[l - 1] = "Buzz"
+            l % 3 == 0 -> res[l - 1] = "Fizz"
+            else -> res[l - 1] = "$l"
+        }
+
+        when {
+            r % 3 == 0 && r % 5 == 0 -> res[r - 1] = "FizzBuzz"
+            r % 5 == 0 -> res[r - 1] = "Buzz"
+            r % 3 == 0 -> res[r - 1] = "Fizz"
+            else -> res[r - 1] = "$r"
+        }
+        r++
+    }
+
+    return res.toMutableList()
 }
