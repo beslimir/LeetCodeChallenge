7e9fdaa5f963973bc89c979b617e32b426ca9c7e
beslimir <marin.beslo@gmail.com>
2022-04-27 08:40:26 +0200

    String 04: Other way (with one hashMap)

--- src/main/kotlin/lessons/string/04_ValidAnagram.kt
+++ src/main/kotlin/lessons/string/04_ValidAnagram.kt
@@ -1,10 +1,39 @@
 package lessons.string
 
 fun main(args: Array<String>) {
-    println(validAnagram("marin", "niram"))
+    println(validAnagram("anagram", "nagaram"))
 }
 
+//other way (with one hashMap)
 fun validAnagram(str1: String, str2: String): Boolean {
+    val myMap: HashMap<Char, Int> = HashMap()
+
+    if (str1.length != str2.length) {
+        return false
+    }
+
+    for (i in str1) {
+        myMap[i] = 1 + myMap.getOrDefault(i, 0)
+    }
+
+    for (i in str2) {
+        if (!myMap.containsKey(i)) {
+            return false
+        } else {
+            myMap[i] = myMap[i]!! - 1
+        }
+    }
+
+    for (i in myMap.values) {
+        if (i != 0) {
+            return false
+        }
+    }
+
+    return true
+}
+
+fun validAnagram2(str1: String, str2: String): Boolean {
     var hashMap1: HashMap<Char, Int> = HashMap()
     var hashMap2: HashMap<Char, Int> = HashMap()
 
