497436fce4edab8662bedd4d622b5a1b307714b9
beslimir <marin.beslo@gmail.com>
2022-04-29 09:09:45 +0200

    String 05: faster way

--- src/main/kotlin/lessons/string/05_GroupAnagrams.kt
+++ src/main/kotlin/lessons/string/05_GroupAnagrams.kt
@@ -1,8 +1,11 @@
 package lessons.string
 
+import java.util.*
+
+
 fun main(args: Array<String>) {
     var array = groupAnagrams(arrayOf("eat", "tea", "tan", "ate", "nat", "bat"))
-    for (i in array) {
+    for (i in array!!) {
         for (j in i) {
             println(j)
         }
@@ -10,7 +13,38 @@ fun main(args: Array<String>) {
     }
 }
 
-fun groupAnagrams(array: Array<String>): Array<Array<String>> {
+//faster version
+fun groupAnagrams(strs: Array<String>): List<List<String>>? {
+    val myArrayList: MutableList<List<String>> = ArrayList()
+    val myMap: MutableMap<String, MutableList<String>> = HashMap()
+
+    for (str in strs) {
+        val key = sortString(str)
+        if (myMap.containsKey(key)) {
+            myMap[key]!!.add(str)
+        } else {
+            val myList: MutableList<String> = ArrayList()
+            myList.add(str)
+            myMap[key] = myList
+        }
+    }
+    for ((key, value) in myMap) {
+        myArrayList.add(value)
+    }
+    return myArrayList
+}
+
+fun sortString(s1: String): String {
+    var str = s1
+    val c1 = str.toCharArray()
+
+    Arrays.sort(c1)
+    str = String(c1)
+
+    return str
+}
+
+fun groupAnagrams2(array: Array<String>): Array<Array<String>> {
     var hashMap1: HashMap<String, Int> = HashMap()
 
     for (str in array) {
