17970e2bccb2b6150f0b50312cee983bd9007850
beslimir <marin.beslo@gmail.com>
2022-05-03 13:59:50 +0200

    Matrix 02: newWay

--- src/main/kotlin/lessons/matrix/02_SpiralMatrix.kt
+++ src/main/kotlin/lessons/matrix/02_SpiralMatrix.kt
@@ -10,7 +10,60 @@ fun main(args: Array<String>) {
     )
 }
 
-fun spiralMatrix(matrix: Array<IntArray>) {
+fun spiralMatrix(matrix: Array<IntArray>): List<Int> {
+    val list = mutableListOf<Int>()
+    val colNumber = matrix[0].size
+    val rowNumber = matrix.size
+    var top = 0
+    var right = colNumber - 1
+    var bottom = rowNumber - 1
+    var left = 0
+    var r = 0
+    var c = -1
+
+    //print
+    println("Before the process")
+    for (r in 0 until rowNumber) {
+        for (c in 0 until colNumber) {
+            print(matrix[r][c])
+        }
+        println()
+    }
+
+    //while the list has fewer elements than the matrix
+    while (list.size < rowNumber * colNumber) {
+        while (r == top && c < right && list.size < rowNumber * colNumber) {
+            c++
+            list.add(matrix[r][c])
+        }
+        top++
+        while (c == right && r < bottom && list.size < rowNumber * colNumber) {
+            r++
+            list.add(matrix[r][c])
+        }
+        right--
+        while (r == bottom && c > left && list.size < rowNumber * colNumber) {
+            c--
+            list.add(matrix[r][c])
+        }
+        bottom--
+        while (c == left && r > top && list.size < rowNumber * colNumber) {
+            r--
+            list.add(matrix[r][c])
+        }
+        left++
+    }
+
+    //print
+    println("After the process")
+    for (i in 0 until list.size) {
+        print(list[i])
+    }
+
+    return list
+}
+
+fun spiralMatrix2(matrix: Array<IntArray>) {
     var list = mutableListOf<Int>()
     var left = 0
     var right = matrix[0].size
