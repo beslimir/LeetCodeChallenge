9264198c67c6fabe417c4d77c67bc1257c1b7935
beslimir <marin.beslo@gmail.com>
2022-05-05 18:59:57 +0200

    Permutations 2nd solution

--- src/main/kotlin/other/Permutations.kt
+++ src/main/kotlin/other/Permutations.kt
@@ -28,3 +28,30 @@ fun permutations(nums: MutableList<Int>): MutableList<MutableList<Int>> {
 
     return myList
 }
+
+//2nd solution
+fun permute(nums: IntArray): List<List<Int>> {
+    val ans =  MutableList<MutableList<Int>>(0){MutableList(0){0}}
+    dfs(ans, nums, 0)
+    return ans
+}
+fun dfs(ans: MutableList<MutableList<Int>>, nums: IntArray, index: Int): Unit{
+    if (index == nums.size){
+        val ls = MutableList<Int>(0){0}
+        for (i in 0 until nums.size){
+            ls.add(nums[i])
+        }
+        ans.add(ls)
+        return
+    }
+    for (i in index until nums.size){
+        swap(nums, i, index)
+        dfs(ans, nums, index+1)
+        swap(nums, i, index)
+    }
+}
+fun swap(nums: IntArray, i: Int, j: Int): Unit{
+    val tmp = nums[i]
+    nums[i] = nums[j]
+    nums[j] = tmp
+}
