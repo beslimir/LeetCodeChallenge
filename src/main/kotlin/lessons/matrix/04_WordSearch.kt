f0f5f7d0f382173acaa0f70a1a7c464ce5d19bfd
beslimir <marin.beslo@gmail.com>
2022-05-09 21:01:34 +0200

    matrix 04: Solution with backtracking (recursion - dfs)

--- src/main/kotlin/lessons/matrix/04_WordSearch.kt
+++ src/main/kotlin/lessons/matrix/04_WordSearch.kt
@@ -73,3 +73,39 @@ private fun find(
 
     return result
 }
+
+//solution with backtracking
+fun wordSearch2(matrix: Array<CharArray>, word: String): Boolean {
+    val rows = matrix.size
+    val cols = matrix[0].size
+    val path: MutableList<Pair<Int, Int>> = mutableListOf()
+
+    fun dfs(r: Int, c: Int, i: Int): Boolean {
+        if (i == word.length) {
+            return true
+        }
+        if (r < 0 || c < 0 || r >= rows || c >= cols || word[i] != matrix[r][c] ||
+            path.contains(Pair(r, c))) {
+            return false
+        }
+        path.add(i, Pair(r, c))
+        val res = (dfs(r + 1, c, i + 1) ||
+                dfs(r - 1, c, i + 1) ||
+                dfs(r, c + 1, i + 1) ||
+                dfs(r, c - 1, i + 1)
+                )
+        path.remove(Pair(r, c))
+
+        return res
+    }
+
+    for (r in 0 until rows) {
+        for (c in 0 until cols) {
+            if (dfs(r, c, 0)) {
+                return true
+            }
+        }
+    }
+
+    return false
+}
